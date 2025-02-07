5fa5caf96aad955e9885aee864d555b413b8cfe3
andrechen77 <101299705+andrechen77@users.noreply.github.com>
2025-02-07 16:16:33 -0600

    part 1

--- software/apps/breadboard/main.c
+++ software/apps/breadboard/main.c
@@ -16,9 +16,9 @@
 // Digital outputs
 // Breakout pins 13, 14, and 15
 // These are GPIO pin numbers that can be used in nrf_gpio_* calls
-#define LED_RED   EDGE_P13
-#define LED_GREEN EDGE_P14
-#define LED_BLUE  EDGE_P15
+#define LED_RED   EDGE_P8
+#define LED_GREEN EDGE_P10
+#define LED_BLUE  EDGE_P9
 
 // Digital inputs
 // Breakout pin 16
@@ -45,9 +45,71 @@ static void gpio_init(void);
 static void adc_init(void);
 static float adc_sample_blocking(uint8_t channel);
 
+static float voltage_to_resistance(float voltage) {
+  float other_resistor = 10000;
+
+  if (voltage < 0) {
+    voltage = 0;
+  }
+
+  return (other_resistor * 3.3) / voltage - other_resistor;
+}
+
+static float resistance_to_temp(float resistance) {
+  float beta = 3470.0;
+  float r_0 = 10000.0;
+  float t_0 = 298.15;
+
+  float r_inf = r_0 * exp(-beta / t_0);
+
+  float kelvin = beta / log(resistance / r_inf);
+
+  return kelvin - 273.15;
+}
+
+// returns 0 for dark, 1 for bright, and 2 for very bright
+static int voltage_to_light_level(float voltage) {
+  if (voltage < 2.0) {
+    return 0;
+  } else if (voltage < 3.0) {
+    return 1;
+  } else {
+    return 2;
+  }
+}
+
 static void sample_timer_callback(void* _unused) {
   // Do things periodically here
-  // TODO
+
+  // read switch
+  int switch_value = nrf_gpio_pin_read(SWITCH_IN);
+  printf("switch value is %d\n", switch_value);
+  if (switch_value) {
+    nrf_gpio_pin_write(LED_BLUE, 0);
+  } else {
+    nrf_gpio_pin_write(LED_BLUE, 1);
+  }
+
+  float value = adc_sample_blocking(ADC_LIGHT_CHANNEL);
+  printf("light sensor value is %f\n", value);
+  int light_level = voltage_to_light_level(value);
+  if (light_level > 0) {
+    nrf_gpio_pin_write(LED_RED, 0);
+  } else {
+    nrf_gpio_pin_write(LED_RED, 1);
+  }
+
+  float temp_voltage = adc_sample_blocking(ADC_TEMP_CHANNEL);
+  // printf("temperature sensor voltage is %f\n", temp_voltage);
+  float temp_resistance = voltage_to_resistance(temp_voltage);
+  // printf("temperature sensor resistance is %f\n", temp_resistance);
+  float temp_celsius = resistance_to_temp(temp_resistance);
+  printf("temperature sensor value is %f\n", temp_celsius);
+  if (temp_celsius > 29.0) {
+    nrf_gpio_pin_write(LED_GREEN, 0);
+  } else {
+    nrf_gpio_pin_write(LED_GREEN, 1);
+  }
 }
 
 static void saadc_event_callback(nrfx_saadc_evt_t const* _unused) {
@@ -57,13 +119,17 @@ static void saadc_event_callback(nrfx_saadc_evt_t const* _unused) {
 
 static void gpio_init(void) {
   // Initialize output pins
-  // TODO
+  nrf_gpio_pin_dir_set(LED_RED, NRF_GPIO_PIN_DIR_OUTPUT);
+  nrf_gpio_pin_dir_set(LED_BLUE, NRF_GPIO_PIN_DIR_OUTPUT);
+  nrf_gpio_pin_dir_set(LED_GREEN, NRF_GPIO_PIN_DIR_OUTPUT);
 
   // Set LEDs off initially
-  // TODO
+  nrf_gpio_pin_write(LED_RED, 1);
+  nrf_gpio_pin_write(LED_BLUE, 1);
+  nrf_gpio_pin_write(LED_GREEN, 1);
 
   // Initialize input pin
-  // TODO
+  nrf_gpio_pin_dir_set(SWITCH_IN, NRF_GPIO_PIN_DIR_INPUT);
 }
 
 static void adc_init(void) {
@@ -95,18 +161,20 @@ static float adc_sample_blocking(uint8_t channel) {
   ret_code_t error_code = nrfx_saadc_sample_convert(channel, &adc_counts);
   APP_ERROR_CHECK(error_code);
 
+  // printf("ADC counts: %d\n", adc_counts);
+
   // convert ADC counts to volts
   // 12-bit ADC with range from 0 to 3.6 Volts
-  // TODO
+  float voltage = ((float)adc_counts / 4096.0) * 3.6;
 
   // return voltage measurement
-  return 0.0;
+  return voltage;
 }
 
 
 int main(void) {
   printf("Board started!\n");
-  
+
   // initialize GPIO
   gpio_init();
 
@@ -119,7 +187,7 @@ int main(void) {
 
   // start timer
   // change the rate to whatever you want
-  app_timer_start(sample_timer, 32768, NULL);
+  app_timer_start(sample_timer, 32768 / 2, NULL);
 
   // loop forever
   while (1) {
